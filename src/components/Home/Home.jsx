64afee7642ac68f97557c8a5cb829379c88919ba
Nicola Winterson <nicola.l.winterson@gmail.com>
2020-06-23 18:35:07 +0200

    Updated CSS to mobile home view

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import MenuIcon from '@material-ui/icons/Menu';
 import {
     Link
   } from "react-router-dom";
 import "./styles.css";
 
 const Home = () => {
+
+    const sayHello = () => {
+        alert('Hello!');
+    }
+
     return (
         <div className="home__container">
             <nav>
@@ -15,6 +21,15 @@ const Home = () => {
                         <Link to="/cv">CV</Link>
                     </li>
                     <li>
+                        <Link to="/portfolio">PORTFOLIO</Link>
+                    </li>
+                </ul>
+                <ul className="home_navbar--mobile">
+                    <MenuIcon className="home_navbar--mobile-icon" onClick={sayHello}/>
+                    <li className="home_navbar--mobile-li">
+                        <Link to="/cv">CV</Link>
+                    </li>
+                    <li className="home_navbar--mobile-li">
                         <Link to="/portfolio">PORTFOLIO</Link>
                     </li>
                 </ul>
