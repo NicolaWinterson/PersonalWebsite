858b3f6854fbfe983888a814e73cab82c463ead9
NicolaWinterson <nicola.l.winterson@gmail.com>
2020-10-21 10:37:06 +0200

    added screenshots for portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,8 +14,8 @@ const Portfolio = () => {
                     <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="Folkets Pops1" link="https://folketspops.se/" />
                     <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="To do list" link="https://codepen.io/nicolawinterson/pen/xxbZqEq" />
                     <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="Tic Tac Toe w/ hooks" link="https://codesandbox.io/s/tick-tack-toe-whooks-v1-xmjpj" />
-                    <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="Receipt search API call" link="https://recipesearchappreact.netlify.app/" />
-                    <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="Weather app API call" link="https://codesandbox.io/s/weather-api-app-v6-dmgnc/" />
+                    <PortfolioItem imgsrc="../Screenshot_2020-10-21-React-Recipe-App.png" alt="Receipt search API call" link="https://recipesearchappreact.netlify.app/" />
+                    <PortfolioItem imgsrc="../Screenshot_2020-10-21-React-Weather-App.png" alt="Weather app API call" link="https://codesandbox.io/s/weather-api-app-v6-dmgnc/" />
                     <PortfolioItem imgsrc="https://getuikit.com/v2/docs/images/placeholder_600x400.svg" alt="Folkets Pops6" link="https://folketspops.se/" />
                 </div>
             </div>
